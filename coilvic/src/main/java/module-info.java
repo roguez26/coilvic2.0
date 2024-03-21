bcccb41764292baa74c6476cd7f5142223638cb8
ivanr <ivanr@roguez>
2024-03-21 10:17:16 -0600

    Merge origin/master
    Conflicts:
    	src/mx/fei/coilvicapp/logic/representante/InstitutionalRepresentativeDAO.java

--- coilvic/src/main/java/module-info.java
+++ coilvic/src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module coilvic {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.logging.log4j;
+    requires java.sql;
 
     opens coilvic.controllers to javafx.fxml;
     exports coilvic.controllers;
+    exports coilvic.objects;
 }
