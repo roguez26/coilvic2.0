d269740758650125b5a7a1230de56e9f4aae8afd
francoroguez <118698321+roguez26@users.noreply.github.com>
2024-05-21 00:22:40 -0600

    feedback r

---
+++ javaapplication2/src/mx/fei/coilvicapp/logic/feedback/IFeedback.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package mx.fei.coilvicapp.logic.feedback;
+
+import java.util.ArrayList;
+import mx.fei.coilvicapp.logic.implementations.DAOException;
+
+/**
+ *
+ * @author ivanr
+ */
+public interface IFeedback {
+    
+    public int registerQuestion(Question question) throws DAOException;
+    
+    public int deleteQuestion(Question question) throws DAOException;
+    
+    public int registerStudentResponses(ArrayList<Response> responses) throws DAOException;
+    
+    public int updateQuestionTransaction(Question question) throws DAOException;
+    
+    public ArrayList<Question> getQuestionByType(String type) throws DAOException;
+    
+    public boolean areThereStudentQuestions() throws DAOException;
+    
+}
