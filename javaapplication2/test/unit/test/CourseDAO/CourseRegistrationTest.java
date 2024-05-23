449172d0c02b2caa699a50cfeda8583b9631c568
EdgarDidier Montiel Acosta <162669078+d0ubl3dd@users.noreply.github.com>
2024-05-23 13:54:01 -0600

    Update CourseRegistrationTest.java

--- javaapplication2/test/unit/test/CourseDAO/CourseRegistrationTest.java
+++ javaapplication2/test/unit/test/CourseDAO/CourseRegistrationTest.java
@@ -20,10 +20,12 @@ import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
 
-public class CourseRegistrationTest {
+public class CourseDAORegistrationTest {
     
     private static final CourseDAO COURSE_DAO = new CourseDAO();
     private static final Course COURSE_FOR_TESTING = new Course();
+    
+    private static final Course AUX_COURSE = new Course();
     
     private static final ProfessorDAO PROFESSOR_DAO = new ProfessorDAO();
     private static final Professor AUX_PROFESSOR = new Professor();
@@ -41,7 +43,7 @@ public class CourseRegistrationTest {
     private static final Term AUX_TERM = new Term();
     
     @Before
-    public void setUp() throws DAOException {
+    public void setUp() throws DAOException {        
         int idProfessor = 0;
         int idCountry = 0;
         int idUniversity = 0;
@@ -54,6 +56,7 @@ public class CourseRegistrationTest {
         initializeAuxiliarLanguage();
         initializeAuxiliarTerm();
         intializeCourse();
+        intializeAuxiliaryCourse();
         
         try {
             idCountry = COUNTRY_DAO.registerCountry(AUX_COUNTRY);
@@ -62,16 +65,16 @@ public class CourseRegistrationTest {
             AUX_PROFESSOR.setIdUniversity(idUniversity);
             idProfessor = PROFESSOR_DAO.registerProfessor(AUX_PROFESSOR);
             idLanguage = LANGUAGE_DAO.registerLanguage(AUX_LANGUAGE);
-            idTerm = TERM_DAO.registerTerm(AUX_TERM);
-            
+            idTerm = TERM_DAO.registerTerm(AUX_TERM);                                          
         } catch (DAOException exception) {
-            Logger.getLogger(CourseRegistrationTest.class.getName()).log(Level.SEVERE, null, exception);
+            Logger.getLogger(CourseDAORegistrationTest.class.getName()).log(Level.SEVERE, null, exception);
         }
         AUX_COUNTRY.setIdCountry(idCountry);
         AUX_UNIVERSITY.setIdUniversity(idUniversity);
         AUX_PROFESSOR.setIdProfessor(idProfessor);
         AUX_LANGUAGE.setIdLanguage(idLanguage);
         AUX_TERM.setIdTerm(idTerm); 
+        AUX_COURSE.setIdCourse(registerAuxiliaryCourse());
     }
     
     private void initializeAuxiliarProfessor() {
@@ -100,7 +103,7 @@ public class CourseRegistrationTest {
     }
     
     private void initializeAuxiliarTerm() {
-        AUX_TERM.setName("Febrero-Junio 2024");        
+        AUX_TERM.setName("Agosto2024–Enero2025");       
     }
     
     private void intializeCourse() {
@@ -108,7 +111,7 @@ public class CourseRegistrationTest {
         COURSE_FOR_TESTING.setLanguage(AUX_LANGUAGE);
         COURSE_FOR_TESTING.setTerm(AUX_TERM);
         COURSE_FOR_TESTING.setName("Programacion");
-        COURSE_FOR_TESTING.setGeneralObjective("Los alumnos comprendan la"
+        COURSE_FOR_TESTING.setGeneralObjective("Los alumnos comprenden la"
         + " programacion estructurada y orientada a objetos");
         COURSE_FOR_TESTING.setTopicsInterest("Abstraccion, Herencia, Polimorfismo");
         COURSE_FOR_TESTING.setNumberStudents(35);
@@ -116,32 +119,108 @@ public class CourseRegistrationTest {
         COURSE_FOR_TESTING.setAdditionalInformation("Ademas de la progrmacion"
         + " orientada a objetos veremos el paradigma funcional");
     }
-
+    
+    private void intializeAuxiliaryCourse() {
+        AUX_COURSE.setProfessor(AUX_PROFESSOR);
+        AUX_COURSE.setLanguage(AUX_LANGUAGE);
+        AUX_COURSE.setTerm(AUX_TERM);
+        AUX_COURSE.setName("Principios de Diseño de Software");
+        AUX_COURSE.setGeneralObjective("Los alumnos obtienen saberes"
+        + " tanto teoricos y practicos sobre el diseño de software");
+        AUX_COURSE.setTopicsInterest("Patrones de diseño,"
+        + " Metodologias de desarrollo de software, ICONIX ");
+        AUX_COURSE.setNumberStudents(40);
+        AUX_COURSE.setStudentsProfile("Ingenieria de software");                              
+        AUX_COURSE.setAdditionalInformation("Trabajo de forma presencial"
+        + " y combino el empleo de material didáctico digital, asi como actividades"
+        + " en la plataforma institucional de la Universidad Veracruzana: Eminus");
+    }
+    
+    private int registerAuxiliaryCourse() {
+        int idAuxCourse = 0;
+        try {
+            idAuxCourse = COURSE_DAO.registerCourse(AUX_COURSE); 
+        } catch (DAOException exception) {
+            Logger.getLogger(CourseDAORegistrationTest.class.getName()).log(Level.SEVERE, null, exception);
+        }
+        return idAuxCourse;
+    }
+    
     @Test
-    public void testInsertCourseSucces() {
+    public void testRegisterCourseSucces() {
         int result = 0;
         
         try {
             result = COURSE_DAO.registerCourse(COURSE_FOR_TESTING);
             COURSE_FOR_TESTING.setIdCourse(result);
         } catch (DAOException exception) {
-            Logger.getLogger(CourseRegistrationTest.class.getName()).log(Level.SEVERE, null, exception);
-        }
-        
+            Logger.getLogger(CourseDAORegistrationTest.class.getName()).log(Level.SEVERE, null, exception);
+        }        
         assertTrue(result > 0);     
+    }
+    
+    @Test
+    public void testRegisterCourseFailByDuplicateCourse() {
+        int result = 0;
+        COURSE_FOR_TESTING.setName(AUX_COURSE.getName());    
+        try {
+            result = COURSE_DAO.registerCourse(COURSE_FOR_TESTING);            
+        } catch (DAOException exception) {
+            Logger.getLogger(CourseDAORegistrationTest.class.getName()).log(Level.SEVERE, null, exception);
+            System.out.println(exception.getMessage());
+        }
+        COURSE_FOR_TESTING.setIdCourse(result);
+        assertTrue(result > 0);
+    }
+    
+    @Test
+    public void testRegisterCourseFailByNonExistenceLanguage() {
+        int result = 0;
+        int idLanguage;
+        
+        idLanguage = COURSE_FOR_TESTING.getLanguage().getIdLanguage();
+        COURSE_FOR_TESTING.getLanguage().setIdLanguage(-1);
+        try {
+            result = COURSE_DAO.registerCourse(COURSE_FOR_TESTING);            
+        } catch(DAOException exception) {
+            Logger.getLogger(CourseDAORegistrationTest.class.getName()).log(Level.SEVERE, null, exception);
+            System.out.println(exception.getMessage());
+        }        
+        COURSE_FOR_TESTING.setIdCourse(result);        
+        COURSE_FOR_TESTING.getLanguage().setIdLanguage(idLanguage);
+        assertTrue(result > 0);
+    }
+    
+    @Test
+    public void testRegisterCourseFailByNonExistenceTerm() {
+        int result = 0;
+        int idTerm;
+        
+        idTerm = COURSE_FOR_TESTING.getTerm().getIdTerm();
+        COURSE_FOR_TESTING.getTerm().setIdTerm(-1);        
+        try {
+            result = COURSE_DAO.registerCourse(COURSE_FOR_TESTING);            
+        } catch(DAOException exception) {
+            Logger.getLogger(CourseDAORegistrationTest.class.getName()).log(Level.SEVERE, null, exception);
+            System.out.println(exception.getMessage());
+        }        
+        COURSE_FOR_TESTING.setIdCourse(result);
+        COURSE_FOR_TESTING.getTerm().setIdTerm(idTerm);
+        assertTrue(result > 0);
     }
     
     @After
     public void tearDown() {
         try {
             COURSE_DAO.deleteCourseByIdCourse(COURSE_FOR_TESTING.getIdCourse());
+            COURSE_DAO.deleteCourseByIdCourse(AUX_COURSE.getIdCourse());            
             PROFESSOR_DAO.deleteProfessorByID(AUX_PROFESSOR.getIdProfessor());
             UNIVERSITY_DAO.deleteUniversity(AUX_UNIVERSITY.getIdUniversity());
             COUNTRY_DAO.deleteCountry(AUX_COUNTRY.getIdCountry());
-            LANGUAGE_DAO.deleteLanguage(AUX_LANGUAGE.getIdLanguage());
-            TERM_DAO.deleteTerm(AUX_TERM.getIdTerm());
+            //LANGUAGE_DAO.deleteLanguage(AUX_LANGUAGE.getIdLanguage());
+            //TERM_DAO.deleteTerm(AUX_TERM.getIdTerm());
         } catch (DAOException exception) {
-            Logger.getLogger(CourseRegistrationTest.class.getName()).log(Level.SEVERE, null, exception);
+            Logger.getLogger(CourseDAORegistrationTest.class.getName()).log(Level.SEVERE, null, exception);
         }
     }
 }
